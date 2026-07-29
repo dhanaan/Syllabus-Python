f821438e1ef8e5703d070fabd8640489c84c4d02
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-29 18:17:56 +0700

    fix print statements wording

--- range.py
+++ range.py
@@ -1,11 +1,11 @@
 def is_in_range(num: int):
     if not num >= 1:
-        return 'the number must be more than one!'
+        return 'the number must be greater of equal to one!'
 
     if not num <= 100:
-        return 'the number must be greater than a hundred!'
+        return 'the number must be smaller or equal to a hundred!'
 
-    return f'{num} is in range 1 to 100, ok have a nice day'
+    return f'{num} is in range of 1 to 100, good'
 
 usr_input = input('choose a number from range 1 to 100: ')
 try:
