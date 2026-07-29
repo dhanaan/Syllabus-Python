f51c1c2a6071afae33c08aad971cb6e7abafe269
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-29 10:24:13 +0700

    use variables, data types and type casting

---
+++ variables.py
@@ -0,0 +1,8 @@
+name = 'Dhanan'
+age = 16
+height = 175.1
+male = True
+
+print(f'{name} is a {age} years old {"male" if male else "female"} with a height of {height}')
+weight = float(input("what about weight? (in kg): "))
+print(f'{name}\'s weight is {weight}kg')
