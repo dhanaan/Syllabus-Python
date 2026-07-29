5ddb18bd819d511bff8389132fba62c3fd1f805e
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-29 10:48:45 +0700

    square root tool

---
+++ userInput.py
@@ -0,0 +1,5 @@
+from math import sqrt
+usr_input = float(input('get the square root of: '))
+sqt_answer = sqrt(usr_input)
+print(f'the square root of {usr_input} is {sqt_answer}')
+print(f'because {sqt_answer} times {sqt_answer} is {sqt_answer * sqt_answer}')
