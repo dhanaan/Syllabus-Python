179c5b947c71afc9c9d3b2ba7ffb23e352f8eae4
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-30 10:57:14 +0700

    sqrt tool use take_input module now

--- userInput.py
+++ userInput.py
@@ -1,5 +1,7 @@
 from math import sqrt
-usr_input = float(input('get the square root of: '))
+from take_input import take_input
+
+usr_input = take_input('get the square root of: ', allow_type=float)
 sqt_answer = sqrt(usr_input)
 print(f'the square root of {usr_input} is {sqt_answer}')
 print(f'because {sqt_answer} times {sqt_answer} is {sqt_answer * sqt_answer}')
