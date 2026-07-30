31f56b9ab5593e193818e5fc11f9bca14f602936
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-30 10:02:07 +0700

    take_input.py : a lib for taking input. weight and temperature basic prototype done

--- weight-temperature.py
+++ weight-temperature.py
@@ -1,30 +1,63 @@
-def take_input(text: str, choices: list):
-    usr_input = input(text)
-    if usr_input in choices:
-        return usr_input
-    else:
-        print(f'invalid, please choose between {choices}')
-        return take_input(text, choices)
-
+import take_input as inp
 
 mode = ''
 weight_unit = ['g', 'kg', 'mg', 'lb', 'oz', 't']
-
 to_grams = [1, 1000, 0.001, 453.592, 28.3495, 1000000]
 
+temp_unit = ['C', 'F', 'K', 'R']
 
-temp_unit = ['C', 'F', 'K']
-mode = take_input('convert weight/temperature (w/t): ', ['w', 't'])
-if mode == 'w':
-    unit = take_input(f'unit to convert {weight_unit}: ', weight_unit)
-else:
-    unit = take_input(f'unit to convert {temp_unit}: ', temp_unit)
 
-amount = float(input('amount to convert: '))
+def convert_temperature(amount: float, unit: str, unit_to: str) -> float:
+    '''Convert temperature between celcius, fahrenheit, kelvin and rankine.
+    Input must be their first letter uppercase
+    '''
 
-if mode == 'w':
-    print(f'{amount} {unit} is also equal to')
+    unit = unit.upper()
+    unit_to = unit_to.upper()
+
+    # Convert to Celcius first
+    if unit == 'C':
+        celsius = amount
+    elif unit == 'K':
+        celsius = amount - 273.15
+    elif unit == 'F':
+        celsius = (amount - 32) * 5/9
+    elif unit == 'R':
+        celsius = (amount - 491.67) * 5/9
     
-else:
-    pass
+    # Convert from Celsius to target unit
+    if unit_to == 'C':
+        return celsius
+    elif unit_to == 'K':
+        return celsius + 273.15
+    elif unit_to == 'F':
+        return (celsius * 9/5) + 32
+    elif unit_to == 'R':
+        return (celsius * 9/5) + 491.67
 
+mode = inp.take_input('Convert Weight/Temperature (w/t): ', ['w', 't'], case_sensitive=False)
+if mode == 'w':
+    unit = inp.take_input(f'Unit to convert {weight_unit}: ', weight_unit, case_sensitive=False)
+else:
+    unit = inp.take_input(f'Unit to convert {temp_unit}: ', temp_unit, case_sensitive=False)
+
+amount = inp.take_input('Amount to convert: ', allow_type=float)
+
+print('\n-----------------------')
+
+if mode == 'w':
+    idx = weight_unit.index(unit)
+    grams = amount * to_grams[idx]
+    print(f'{amount} {unit} is also equal to')
+    for i, curr_unit in enumerate(weight_unit):
+        if curr_unit != unit:
+            result = grams / to_grams[i]
+            print(f'{result} {curr_unit}')
+else:
+    print(f'{amount} {unit.upper()} is also equal to')
+    for i, curr_unit in enumerate(temp_unit):
+        if curr_unit != unit:
+            result = convert_temperature(amount, unit, curr_unit.upper())
+            print(f'{result:,.1f} {curr_unit}')
+
+print('-----------------------\n')
