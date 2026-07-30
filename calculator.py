b50973e5c51e7eefea0d1efa47ebc51305f73a11
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-30 11:01:15 +0700

    change how calculator use take_input module

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
-import take_input as inp
+from take_input import take_input
 
 available_operator = ['+', '-', '*', '/', '**', '//', '%']
-operator = inp.take_input(f"Enter an operator {available_operator}: ", choices=available_operator)
-num1 = inp.take_input("Enter the 1st number: ", allow_type=float)
-num2 = inp.take_input("Enter the 2nd number: ", allow_type=float)
+operator = take_input(f"Enter an operator {available_operator}: ", choices=available_operator)
+num1 = take_input("Enter the 1st number: ", allow_type=float)
+num2 = take_input("Enter the 2nd number: ", allow_type=float)
 
 if operator == "+":
     result = num1 + num2
