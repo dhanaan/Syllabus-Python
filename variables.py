f03426a6cbda0cbde49a5c967b392fc7c2d16fe8
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-30 12:54:15 +0700

    use take_input in variables.py

--- variables.py
+++ variables.py
@@ -1,8 +1,10 @@
+from take_input import take_input
+
 name = 'Dhanan'
 age = 16
 height = 175.1
 male = True
 
 print(f'{name} is a {age} years old {"male" if male else "female"} with a height of {height}')
-weight = float(input("what about weight? (in kg): "))
+weight = take_input("what about weight? (in kg): ", allow_type=int)
 print(f'{name}\'s weight is {weight}kg')
