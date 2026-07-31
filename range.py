3fd431427dea4802860720708ea0f384a1023d4f
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-07-31 09:57:26 +0700

    rename sqrt_tool and use take_input in range.py

--- range.py
+++ range.py
@@ -1,3 +1,5 @@
+from take_input import take_input
+
 def is_in_range(num: int):
     if not num >= 1:
         return 'the number must be more than one!'
@@ -7,9 +9,6 @@ def is_in_range(num: int):
 
     return f'{num} is in range 1 to 100, ok have a nice day'
 
-usr_input = input('choose a number from range 1 to 100: ')
-try:
-    usr_input = int(usr_input)
-    print(is_in_range(usr_input))
-except:
-    print('that is NOT an integer')
+usr_input = take_input('choose a number from range 1 to 100: ', allow_type=int)
+print(is_in_range(usr_input))
+
--- sqrt_tool.py
+++ sqrt_tool.py
@@ -3,5 +3,5 @@ from take_input import take_input
 
 usr_input = take_input('get the square root of: ', allow_type=float)
 sqt_answer = sqrt(usr_input)
-print(f'the square root of {usr_input} is {sqt_answer}')
-print(f'because {sqt_answer} times {sqt_answer} is {sqt_answer * sqt_answer}')
+print(f'the square root of {usr_input} is {sqt_answer:.2f}')
+print(f'because {sqt_answer:.2f} times {sqt_answer:.2f} is {(sqt_answer * sqt_answer):.2f}')
