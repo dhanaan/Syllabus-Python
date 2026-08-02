92de5cb8f9c73ef3c733287ce6f4f9befe273dbc
ariwishrc <ariwishrc@gmail.com>
2026-08-02 22:55:08 +0800

    fruit market finished

--- fruit_market.py
+++ fruit_market.py
@@ -1,3 +1,5 @@
+import take_input as inp
+
 fruits = {
     'apple': 3.00,
     'banana': 2.50,
@@ -10,4 +12,25 @@ fruits = {
     'pear': 3.80,
     'kiwi': 7.20
 }
+
+
+total = 0
+def show_menu():
+    print('\nFRUIT LIST\n=======')
+    for fruit in fruits:
+        print(f'{fruit}: {fruits.get(fruit):,.2f}/kg')
+    print()
+
+
+order = True
+while order:
+    show_menu()
+    fruit_curr = inp.take_input("Which fruit would you like to buy? ", choices=list(fruits.keys()),case_sensitive=False)
+    fruit_grams = inp.take_input("How much do you want? (in grams): ", allow_type=float)
+    price = (fruit_grams / 1000) * fruits[fruit_curr]
+    print(f"+{fruit_grams}g of {fruit_curr}, total will +${price:,.2f}")
+    total += price
+    order = True if inp.take_input("Order more? (y/n): ", choices=['y', 'n'], case_sensitive=False) == 'y' else False
+
+print(f'Your total is ${total:,.2f}')
 
