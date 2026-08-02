490b5cd689b8e6dd51ba39d0682f00698a29f1ce
ariwishrc <ariwishrc@gmail.com>
2026-08-02 22:32:27 +0800

    added fruits

---
+++ fruit_market.py
@@ -0,0 +1,13 @@
+fruits = {
+    'apple': 3.00,
+    'banana': 2.50,
+    'orange': 4.00,
+    'grape': 6.50,
+    'mango': 5.00,
+    'pineapple': 3.50,
+    'watermelon': 1.80,
+    'strawberry': 8.00,
+    'pear': 3.80,
+    'kiwi': 7.20
+}
+
