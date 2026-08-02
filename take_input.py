77265ea7327d2f380cb5831e45264a7ef52ec488
ariwishrc <ariwishrc@gmail.com>
2026-08-02 23:06:35 +0800

    type checking now done in a different func for cleaner code

--- take_input.py
+++ take_input.py
@@ -1,25 +1,13 @@
-def take_input(text: str = "", rule = lambda x: True, choices: list = [], allow_type = str,  case_sensitive: bool = True, rule_error: str = "Rule violated"):
+__all__ = ["take_input"]
+
+def take_input(text: str = "", allow_type = str, case_sensitive: bool = True, choices: list = [], rule = lambda x: True, rule_error: str = "Rule violated"):
     '''just the default input() module but with extended capabilities like: type checking, case sensitive check, choices list'''
     usr_input = input(text)
     
-    # Convert type first
-    if allow_type == str:
-        converted = usr_input
-    elif allow_type == int:
-        try:
-            converted = int(usr_input)
-        except ValueError:
-            print(f'Invalid, {usr_input} is not a number')
-            return take_input(text=text, rule=rule, choices=choices, allow_type=allow_type, case_sensitive=case_sensitive, rule_error=rule_error)
-    elif allow_type == float:
-        try:
-            converted = float(usr_input)
-        except ValueError:
-            print(f'Invalid, {usr_input} is not a number')
-            return take_input(text=text, rule=rule, choices=choices, allow_type=allow_type, case_sensitive=case_sensitive, rule_error=rule_error)
-    else:
-        raise ValueError(f'Unsupported type: {allow_type}')
+    converted = _check_type(usr_input, allow_type)
     
+    if converted == False:
+        return take_input(text=text, rule=rule, choices=choices, allow_type=allow_type, case_sensitive=case_sensitive, rule_error=rule_error)
     # Now apply rule to converted value
     try:
         rule(converted)
@@ -45,3 +33,21 @@ def take_input(text: str = "", rule = lambda x: True, choices: list = [], allow_
     else:
         print(rule_error)
         return take_input(text=text, rule=rule, choices=choices, allow_type=allow_type, case_sensitive=case_sensitive, rule_error=rule_error)
+
+def _check_type(usr_input, allow_type):
+    if allow_type == str:
+        return usr_input
+    elif allow_type == int:
+        try:
+            return int(usr_input)
+        except ValueError:
+            print(f'Invalid, {usr_input} is not a number')
+            return False
+    elif allow_type == float:
+        try:
+            return float(usr_input)
+        except ValueError:
+            print(f'Invalid, {usr_input} is not a number')
+            return False
+    else:
+        raise ValueError(f'Unsupported type: {allow_type}')
