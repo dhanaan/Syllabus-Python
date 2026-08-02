89faffbb7d55af3fcd01ded7e04ef6775d596ae9
ariwishrc <ariwishrc@gmail.com>
2026-08-02 22:19:41 +0800

    finished fast_food ordering system with 2D list usage

--- fast_food.py
+++ fast_food.py
@@ -1,4 +1,4 @@
-from take_input import take_input
+import take_input as inp
 
 items = [
     ["Pizza", "Burger", "Fried Chicken"],
@@ -11,20 +11,29 @@ prices = [
     [2.50, 2.50, 1.50],
     [3.99, 2.99, 2.49]
 ]
+result = 0
 
-print('ITEM LIST')
-col = 0
-for item_list, price_list in zip(items, prices):
-    col += 1
-    for item, price in zip(item_list, price_list):
-        print(f'{item} (${price:.2f})', end=' | ')
-    print(f'(COLUMN {col})')
-print()
+def show_menu():
+    print('\nITEM LIST')
+    col = 0
+    for item_list, price_list in zip(items, prices):
+        col += 1
+        for item, price in zip(item_list, price_list):
+            print(f'{item} (${price:.2f})', end=' | ')
+        print(f'(COLUMN {col})')
+    print()
 
-order_col = take_input("Which column would you like to choose? ", allow_type=int, rule= lambda x: True if 0 < x <= len(items) else False, rule_error='Invalid column')
-order_row = take_input("Which row would you like to choose? ", allow_type=int, rule= lambda x: True if 0 < x <= len(items[0]) else False, rule_error='Invalid row')
-order_amount = take_input("How many do you want? ", allow_type=int)
-order_name = items[order_col - 1][order_row - 1]
-order_price = prices[order_col - 1][order_row - 1]
 
-print(f"For {order_amount} {order_name}, your total will be ${(order_amount * order_price):,.2f}")
+more_order = True
+while more_order:
+    show_menu()
+    order_col = inp.take_input("Which column would you like to choose? ", allow_type=int, rule= lambda x: True if 0 < x <= len(items) else False, rule_error='Invalid column')
+    order_row = inp.take_input("Which row would you like to choose? ", allow_type=int, rule= lambda x: True if 0 < x <= len(items[0]) else False, rule_error='Invalid row')
+    order_amount = inp.take_input("How many do you want? ", allow_type=int)
+    order_name = items[order_col - 1][order_row - 1]
+    order_price = prices[order_col - 1][order_row - 1]
+    print(f"+{order_amount} {order_name}, total will +${(order_amount * order_price):,.2f}")
+    result += order_amount * order_price
+    more_order = True if inp.take_input("Order more? (y/n): ", choices=['y', 'n'], case_sensitive=False) == 'y' else False
+
+print(f'Your total is ${result:,.2f}')
