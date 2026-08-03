13c71f75b7cf2f0ff3769cb9bc8bb199469ad913
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-03 15:51:57 +0700

    added shift args to the caesar cipher func

--- encryption.py
+++ encryption.py
@@ -3,7 +3,7 @@ import string
 
 char_list = list(" " + string.ascii_letters + string.digits + string.punctuation)
 
-def convert():
+def caesar_cipher(shift = 3):
     usr = inp.take_input("Do you want to encrypt/decrypt? (e/d): ", choices=['e', 'd'])
     text = inp.take_input("Text: " )
 
@@ -11,18 +11,18 @@ def convert():
     if usr == 'e':
         for letter in text:
             if letter in char_list:
-                result = result + char_list[(char_list.index(letter) + 3) % len(char_list)]
+                result = result + char_list[(char_list.index(letter) + shift) % len(char_list)]
             else:
                 result = result + letter
     else:
         for letter in text:
             if letter in char_list:
-                result = result + char_list[(char_list.index(letter) - 3) % len(char_list)]
+                result = result + char_list[(char_list.index(letter) - shift) % len(char_list)]
             else:
                 result = result + letter
 
     print(result)
 
-convert()
-while inp.take_input("again? (y/n): ", case_sensitive=False, choices=['y', 'n']).lower() == 'y':
-    convert()
+caesar_cipher()
+while inp.take_input("again? (y/n): ", choices=['y', 'n']) == 'y':
+    caesar_cipher()
