71f096adce1b691cf436d0aa1fdffec84aa64c72
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-03 15:27:15 +0700

    caesar cipher done

--- encryption.py
+++ encryption.py
@@ -1,35 +1,19 @@
 import take_input as inp
 import string
 
-original = list(" " + string.ascii_letters + string.digits + string.punctuation)
+char_list = list(" " + string.ascii_letters + string.digits + string.punctuation)
 
 def convert():
-    char_list = original.copy()
-    usr = inp.take_input("Do you want to encrypt/decrypt? (e/d): ", case_sensitive=False, choices=['e', 'd'])
+    usr = inp.take_input("Do you want to encrypt/decrypt? (e/d): ", choices=['e', 'd'])
     text = inp.take_input("Text: ")
-    passkey = inp.take_input("Passkey: ")
 
-    # Convert passkey into numbers
-    key = [original.index(char) for char in passkey]
-
-    key_idx = 0
-    for idx in range(len(char_list)):
-        swap = (idx + key[key_idx]) % len(char_list)
-        char_list[idx], char_list[swap] = char_list[swap], char_list[idx]
-        key_idx = (key_idx + 1) % len(key)
-
-    if usr.lower() == "e":
-        encrypted = ""
-        for c in text:
-            encrypted += char_list[original.index(c)]
-        print(encrypted)
-
+    if usr == 'e':
+        for letter in text:
+            print (char_list[(char_list.index(letter) + 3) % len(char_list)], end='')
     else:
-        decrypted = ""
-        for c in text:
-            decrypted += original[char_list.index(c)]
-        print(decrypted)
+        for letter in text:
+            print (char_list[(char_list.index(letter) - 3) % len(char_list)], end='')
 
 convert()
-while inp.take_input("again? (y/n): ", case_sensitive=False, choices=['y', 'n']).lower() == 'y':
+while inp.take_input("\nagain? (y/n): ", case_sensitive=False, choices=['y', 'n']).lower() == 'y':
     convert()
