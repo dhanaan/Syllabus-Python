9b1cb823d9e4665391392dc6fd08aace0dd3fc8f
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-03 15:37:05 +0700

    encryption handle char that is not in char_list

--- encryption.py
+++ encryption.py
@@ -5,15 +5,24 @@ char_list = list(" " + string.ascii_letters + string.digits + string.punctuation
 
 def convert():
     usr = inp.take_input("Do you want to encrypt/decrypt? (e/d): ", choices=['e', 'd'])
-    text = inp.take_input("Text: ")
+    text = inp.take_input("Text: " )
 
+    result = ""
     if usr == 'e':
         for letter in text:
-            print (char_list[(char_list.index(letter) + 3) % len(char_list)], end='')
+            if letter in char_list:
+                result = result + char_list[(char_list.index(letter) + 3) % len(char_list)]
+            else:
+                result = result + letter
     else:
         for letter in text:
-            print (char_list[(char_list.index(letter) - 3) % len(char_list)], end='')
+            if letter in char_list:
+                result = result + char_list[(char_list.index(letter) - 3) % len(char_list)]
+            else:
+                result = result + letter
+
+    print(result)
 
 convert()
-while inp.take_input("\nagain? (y/n): ", case_sensitive=False, choices=['y', 'n']).lower() == 'y':
+while inp.take_input("again? (y/n): ", case_sensitive=False, choices=['y', 'n']).lower() == 'y':
     convert()
