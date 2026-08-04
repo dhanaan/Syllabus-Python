d14be95562729fd80a1d038218b2af4494288493
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-04 08:23:30 +0700

    take_input small structure changes

--- take_input.py
+++ take_input.py
@@ -6,12 +6,12 @@ def _check_type(usr_input, allow_type):
     
     try:
         return allow_type(usr_input)
-    except ValueError:
+    except (ValueError, TypeError):
         print(f'Invalid, {usr_input} is not a valid {allow_type.__name__}')
         return None
 
     
-def take_input(text: str = "", allow_type = str, case_sensitive: bool = True, choices: list = [], rule = lambda x: True, rule_error: str = "Rule violated"):
+def take_input(text = "", allow_type = str, case_sensitive = True, choices = None, rule = lambda x: True, rule_error = "Rule violated"):
     '''input() module with extended capabilities like type checking, case sensitive check, choices list and custom rules'''
     while True:
         usr_input = input(text)
@@ -25,21 +25,22 @@ def take_input(text: str = "", allow_type = str, case_sensitive: bool = True, ch
             print(rule_error)
             continue
 
-        if rule_valid:
-            if allow_type != str:
-                return converted
-
-            # str
-            if choices == []:
-                return usr_input
-            if not case_sensitive:
-                usr_input = usr_input.lower()
-                choices = [item.lower() for item in choices]
-            if usr_input in choices:
-                return usr_input
-            else:
-                print(f'Invalid, please choose between ({", ".join(choices)})')
-                continue
-        else:
+        if not rule_valid:
             print(rule_error)
+            continue
+        
+        if allow_type is not str:
+            return converted
+
+        # str
+        if not choices:
+            return usr_input
+        if not case_sensitive:
+            usr_input_lower = usr_input.lower()
+            choices_lower = [item.lower() for item in choices]
+
+        if usr_input_lower in choices_lower:
+            return usr_input
+        else:
+            print(f'Invalid, please choose between ({", ".join(choices)})')
             continue
