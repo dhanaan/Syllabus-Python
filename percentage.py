9f90a3d3291fc6c5802ce38f79c492c30c6b0324
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-04 08:55:43 +0700

    percentage math using kwargs

---
+++ percentage.py
@@ -0,0 +1,8 @@
+def percentage(**kwargs):
+    total = sum(kwargs.values())
+    if total > 0:
+        print(f'Total: {total}')
+        for name, val in kwargs.items():
+            print(f'{name}:{val} ({(val/total) * 100:.1f}%)')
+
+percentage(a = 10, b = 21, c = 39, d = 2, e = 16)
