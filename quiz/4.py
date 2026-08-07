f17586761b5b589a1405de58b6801ab1df0fd06c
I Putu Bagus Dhananjaya Ariwisnawa <IPBagus.DAriwisnawa@mitrais.com>
2026-08-07 15:11:35 +0700

    sort the answer before returning

--- quiz/4.py
+++ quiz/4.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 def top_two(numbers):
-    return [num for num, _ in Counter(sorted(numbers, reverse=True)).most_common(2)]
+    return sorted([num for num, _ in Counter(sorted(numbers, reverse=True)).most_common(2)], reverse=True) # for some reason the counter needs the list to be sorted at first..
 
-the_numbers = [2, 5, 5, 11, 11, 2, 9, 3]
+the_numbers = [2, 5, 5, 2, 3, 2, 2, 11, 11, 2, 9, 5, 2]
 print(top_two(the_numbers))
